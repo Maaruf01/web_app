1c3a9ab10dd68134c7f43d0001951e402a8350a8
Aminu Dzara CSC/16D/4295 <aminudzara@gmail.com>
2021-05-21 17:08:45 +0100

    created database file.

---
+++ data/init.sql
@@ -0,0 +1,21 @@
+CREATE SCHEMA `web_app_db` ;
+
+use `web_app_db` ;
+
+CREATE TABLE `web_app_db`.`users` (
+  `id` INT NOT NULL AUTO_INCREMENT,
+  `firstname` VARCHAR(30) NOT NULL,
+  `lastname` VARCHAR(30) NOT NULL,
+  `age` INT(3) NULL,
+  `gender` ENUM('male', 'female') NULL,
+  `picture` MEDIUMBLOB NULL,
+  `address` VARCHAR(60) NULL,
+  `city` VARCHAR(15) NULL,
+  `state` VARCHAR(15) NULL,
+  `country` VARCHAR(25) NULL,
+  `phone` VARCHAR(20) NULL,
+  `email` VARCHAR(50) NOT NULL,
+  `username` VARCHAR(15) NOT NULL,
+  `password` VARCHAR(150) NOT NULL,
+  `date` TIMESTAMP NOT NULL,
+  PRIMARY KEY (`id`));
