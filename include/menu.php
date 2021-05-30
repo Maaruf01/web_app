3ddb72054053801b3af5ee099ba498e683d22d8b
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-05-30 21:28:16 +0100

    modified

--- include/menu.php
+++ include/menu.php
@@ -1,5 +1,5 @@
 <nav id="nav-menu">
 <ul>
-<li><a href="home.php" <?php if($_GET['currentPage'] == 'home') echo 'class="active"'; ?>>Home</a></li>
+<li><a href="index.php" <?php if($_GET['currentPage'] == 'index') echo 'class="active"'; ?>>Home</a></li>
 </ul>
 </nav>
