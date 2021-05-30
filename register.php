7085bccded852ea0d0b0b639345e2611376de9a0
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-05-30 13:25:54 +0100

    removed label for form input

--- register.php
+++ register.php
@@ -4,26 +4,26 @@
 <script src="js/jquery-3.6.0.js"></script>
 <script src="js/main.js"></script>
 </head>
-<body class="access">
+<body class="login_register-body">
 <div>
 <form  method="post"  id="register">
 <ul>
 		
-			<label for="fname">Firstname: </label>
+		
 			<li><input type="text" name="fname" placeholder="First Name" class="fname"></li>
-			<label for="fname">Lastname: </label>
+			
 			<li><input type="text" name="lname" placeholder="Last Name" class="lname"></li>
-			<label for="email">Email: </label>
+			
 			<li><input type="email" name="email" placeholder="example@gmail.com" class="email"></li>
-			<label for="tel">Tel: </label>
+			
 			<li><input type="text" name="tel" placeholder="+2348031856408" class="tel"></li>
-			<label for="address">Address:</label>
+			
 			<li><input type="text" name="address" placeholder="Address" class="address"></li>
-			<label for="username">username: </label>
+			
 			<li><input type="text" name="username" placeholder="User Name" class="username"></li>
-			<label for="pwd">password: </label>
+			
 			<li><input type="password" name="password" placeholder="Password" class="password"></li>
-			<label>Upload Profile Pic:</label>	
+				
 			<li><input type="file" name="image" id="pic"></li>
 			<li><input type="submit" name="submit" value="Sign-up" class="btn"></li>
 		           </ul>
