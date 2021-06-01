bec36bcfe266c1e3b00c4d8ca392294cc8401002
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-06-02 00:56:03 +0100

    Added event script

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 $(function(){
+    
+    $(".userInfo").attr('readonly', true);
+    $(".cta, #login, #register").slideDown(1500);
 
-$(".cta, #login, #register").slideDown(1500);
-
-    $("#edit-profile").click(function(){
-    $("#save-edit").show();
+    $("#edit-profile").click(function()
+    {
+          $("#save-edit").show();
+          $(".userInfo").attr('readonly', false);
+   
     });
 
 });
