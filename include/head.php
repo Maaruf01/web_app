de9cbffd399a7639cbf7f40c94fdef7ba78c673c
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-06-02 00:54:15 +0100

    css link tag corrected

--- include/head.php
+++ include/head.php
@@ -4,4 +4,4 @@
 <head>
 <meta charset="utf-8">
 <meta name="veiwport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="style.css" />
