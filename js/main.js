e3cb4a59353494d37c7a3890243ad9ffe7e436c5
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-06-04 06:24:54 +0100

    Added script for handling user login

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
 $(function(){
     
     $(".userInfo").attr('readonly', true);
-    $(".cta, #login, #register").slideDown(1500);
+
 
     $("#edit-profile").click(function()
     {
           $("#save-edit").show();
           $(".userInfo").attr('readonly', false);
    
+    });
+
+
+    $('#login').submit(function(event){
+        event.preventDefault()      
+        $('#button').val('Please wait...') 
+
+        $.ajax({
+            url:'auth.php',
+            method:'POST',
+            data:$(this).serialize(),
+            error:err=>{
+                console.log(err)
+                alert('An error occured') 
+                $('#button').val('Login')
+            },
+            success:function(resp){
+                if (resp == 1){
+                    document.location.href= 'profile.php'
+                }
+                else{
+                    alert("Incorrect username or password.")
+                    $('#button').val('Login')
+                }
+            }
+        });
+
     });
 
 });
