d256641c01390c3e6c6a3a9ac1361d9da0b6a548
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-06-05 12:28:37 +0100

    added a sql code to get user firstname after registration

--- add_user.php
+++ add_user.php
@@ -3,14 +3,14 @@
 require "config.php";
 require "common.php";
 
-if (isset($_POST['submit'])) {
+//if (isset($_POST['submit'])) {
 	//if (!hash_equals($_SESSION['csrf'], $_POST['csrf'])) die();
 
 	try {
 		$connection = new pdo($dsn, $username, $password, $options);
 		
         $date = date('Y-m-d H:i:s');
-
+		$usern = $_POST['username'];
         $passw = $_POST['password'];
 
         $hashed_passw = password_hash($passw, PASSWORD_BCRYPT);
@@ -21,8 +21,7 @@ if (isset($_POST['submit'])) {
 			'email' => $_POST['email'], 
 			'phone' => $_POST['tel'], 
 			'address' => $_POST['address'],
-			'picture' => $_POST['image'],
-			'username' => $_POST['username'],
+			'username' => $usern,
 			'password' => $hashed_passw,
 			'date' => $date,
 		);
@@ -37,7 +36,30 @@ if (isset($_POST['submit'])) {
 		$statement = $connection->prepare($sql);
 		$statement->execute($new_user);
 		
+		//get new user first name after registration
+		
+		if ($statement){
+
+		$sql = "SELECT firstname FROM users WHERE username = :username;";
+		
+        $statement = $connection->prepare($sql);
+		$statement->bindParam(':username', $usern, PDO::PARAM_STR);
+        $statement->execute();
+
+		$result = $statement->fetch(PDO::FETCH_ASSOC);
+		$firstn = $result['firstname'];
+		
+		
+			echo $firstn;
+
+			
+		}
+		else{
+			echo 2;
+			
+		}
 	} catch (PDOExeption $error) {
 		echo $sql . "</br>" . $error->getMesage();
+		
 	}
-}
+//}
