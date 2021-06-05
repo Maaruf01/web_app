b4fba5b267f8e61b67e59afcf3aa81d0374be344
Aminu Dzara <aminudzara@gmail.com>
2021-06-05 08:13:57 +0100

    modified database user credential in config.php

--- config.php
+++ config.php
@@ -7,7 +7,7 @@
 
 $host ="localhost";
 $username=  "root";
-$password ="root";
+$password ="";
 $dbname = "web_app_db"; // will use later
 $dsn = "mysql:host=$host;dbname=$dbname"; // will use later
 $options= array (
