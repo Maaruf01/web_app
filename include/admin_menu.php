5cef92f3e01594aca0285a87473cd75dd1b480be
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-07-16 22:29:02 +0100

    added navigation link

--- include/admin_menu.php
+++ include/admin_menu.php
@@ -1,6 +1,7 @@
 <nav id="nav-menu">
 <ul>
 <li><a href="profile.php" <?php if($_GET['currentPage'] == 'profile') echo 'class="active"'; ?>>Profile</a></li>
+<li><a href="post.php" <?php if($_GET['currentPage'] == 'post') echo 'class="active"'; ?>>Post</a></li>
 <li><a href="logout.php">Logout</a></li>
 </ul>
 </nav>
