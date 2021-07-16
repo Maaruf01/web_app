6900adfcc5232112bc75cb6d1907b12646bb71d6
Hafsah Muhammed <hafsamuhd1010@gmail.com>
2021-07-16 17:03:26 +0100

    modified database schema

--- data/init.sql
+++ data/init.sql
@@ -6,8 +6,6 @@ CREATE TABLE `web_app_db`.`users` (
   `id` INT NOT NULL AUTO_INCREMENT,
   `firstname` VARCHAR(30) NOT NULL,
   `lastname` VARCHAR(30) NOT NULL,
-  `age` INT(3) NULL,
-  `gender` ENUM('male', 'female') NULL,
   `picture` MEDIUMBLOB NULL,
   `address` VARCHAR(60) NULL,
   `phone` VARCHAR(20) NULL,
@@ -16,3 +14,32 @@ CREATE TABLE `web_app_db`.`users` (
   `password` VARCHAR(150) NOT NULL,
   `date` TIMESTAMP NOT NULL,
   PRIMARY KEY (`id`));
+
+CREATE TABLE `web_app_db`.`posts` (
+  `id` INT NOT NULL,
+  `title` TINYTEXT NOT NULL,
+  `content` MEDIUMTEXT NOT NULL,
+  `time` TIMESTAMP NOT NULL,
+  `user_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `author_idx` (`user_id` ASC) VISIBLE,
+  CONSTRAINT `author`
+    FOREIGN KEY (`user_id`)
+    REFERENCES `web_app_db`.`users` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
+CREATE TABLE `web_app_db`.`comment` (
+  `id` INT NOT NULL,
+  `name` VARCHAR(45) NOT NULL,
+  `comment` TINYTEXT NOT NULL,
+  `time` TIMESTAMP NOT NULL,
+  `post_id` INT NOT NULL,
+  PRIMARY KEY (`id`),
+  INDEX `p_id_idx` (`post_id` ASC) VISIBLE,
+  CONSTRAINT `p_id`
+    FOREIGN KEY (`post_id`)
+    REFERENCES `web_app_db`.`posts` (`id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION);
+
