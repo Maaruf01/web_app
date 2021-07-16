b3a18d7dc462f66148234200535ee49b6f68327e
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-07-16 22:32:52 +0100

    nagivation menu for login and register added

--- include/menu.php
+++ include/menu.php
@@ -1,5 +1,7 @@
 <nav id="nav-menu">
 <ul>
 <li><a href="index.php" <?php if($_GET['currentPage'] == 'index') echo 'class="active"'; ?>>Home</a></li>
+<li><a href="login.php" <?php if($_GET['currentPage'] == 'login') echo 'class="active"'; ?>>Login</a></li>
+<li><a href="register.php" <?php if($_GET['currentPage'] == 'register') echo 'class="active"'; ?>>Register</a></li>
 </ul>
 </nav>
