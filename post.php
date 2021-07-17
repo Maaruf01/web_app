11d2a596f416191ef2817f2a840e08a504d861fa
Maaruf01 <abdullahimaarufmaksha@gmail.com>
2021-07-17 01:02:30 +0100

    modified layout

--- post.php
+++ post.php
@@ -1,24 +1,32 @@
+<?php
+if (!isset($_SESSION)) {
+	session_start();
+}
+?>
 <?php require 'common.php'; ?>
 
 <?php require 'include/head.php'; ?>
 <?php include 'include/bootstrap.php'; ?>
-<title> Manage Post </title>
+<title> Post </title>
 </head>
 <body>
 <header><h1>DASHBOARD</h1></header>
 <?php $_GET['currentPage'] = 'post'; 
     include 'include/admin_menu.php'?>
+    <div class="container">
     <form action="publish_post" method="POST">
+    <input name="csrf" type="hidden" value="<?php echo escape($_SESSION['csrf']); ?>">
     <div class="form-group">
-<label for="title">Enter Title:</label><br>
-    <input type="text" name="title" placeholder="Add Title">
+  <label for="title">Enter Title:</label><br>
+    <input type="text" name="title" id="title" placeholder="Add Title">
     </div>
 <div class="form-group">
 <label for="post">Write Post Here:</label><br>
-        <textarea name="post" id="" cols="150" rows="18" placeholder="Enter text here..."></textarea>
+        <textarea name="post" id="contents" cols="100" rows="16" placeholder="Enter text here..."></textarea>
         </div>
-        <input type="submit" name="submit" value="Publish">
+        <input type="submit" class="btn" name="submit" value="Publish">
     </form>
+    </div>
 </body>
 <?php
 include 'include/footer.php';
